db0a88c0bd44d6f386f9bc397e29b77cf10d88d5
Adrian O'Grady <elpollouk@gmail.com>
2015-08-26 15:47:54 +0100

    Updated RefVM to be a Chicken Class

--- src/refvm.js
+++ src/refvm.js
@@ -7,15 +7,14 @@
 	"use strict";
 
 	// Default Null IO implementation in case there's no set user IO
-	var NullIO = function () {
-
-	};
-	NullIO.prototype.getch = function () { return -1; };
-	NullIO.prototype.putch = function () { };
+	var NullIO = Chicken.Class(function () {}, {
+		getch: function () { return -1; },
+		putch: function () {}
+	});
 
 
 	// VM Implementation
-	var RefVM = function () {
+	window.RefVM = Chicken.Class(function () {
 
 		var memory = new Int32Array(30000);
 		for (var i = 0; i < memory.length; i++)
@@ -25,95 +24,95 @@
 		this.dp = 0;
 		this.io = new NullIO();
 
-	};
+	}, {
+		load: function RefVM_load(prog) {
 
-	RefVM.prototype.load = function RefVM_load(prog) {
-		if (typeof prog !== "string") throw new Error("Illegal Argument");
+			if (typeof prog !== "string") throw new Error("Illegal Argument");
 
-		// Validate the loops
-		var counter = 0;
-		for (var i = 0; i < prog.length; i++) {
-			switch (prog[i]) {
-				case "[":
-					counter++;
-					break;
+			// Validate the loops
+			var counter = 0;
+			for (var i = 0; i < prog.length; i++) {
+				switch (prog[i]) {
+					case "[":
+						counter++;
+						break;
 
-				case "]":
-					if (counter === 0) throw new Error("Loop closed but not opened");
-					counter--;
-					break;
+					case "]":
+						if (counter === 0) throw new Error("Loop closed but not opened");
+						counter--;
+						break;
+				}
 			}
-		}
-		if (counter !== 0) throw new Error("Loop opened but not closed");
+			if (counter !== 0) throw new Error("Loop opened but not closed");
 
-		this._prog = prog;
-	};
+			this._prog = prog;
 
-	RefVM.prototype.execute = function RefVM_execute() {
-		
-		var ip = 0;
-		var prog = this._prog;
-		var loopCounter = 0;
+		},
 
-		while (ip < prog.length)
-		{
-			var code = prog[ip];
+		execute: function RefVM_execute() {
+			
+			var ip = 0;
+			var prog = this._prog;
+			var loopCounter = 0;
 
-			switch (code) {
-				case ">":
-					if (this.dp === (this.memory.length-1)) throw new RangeError("Attempted to move beyond upper limit of memory");
-					this.dp++;
-					break;
+			while (ip < prog.length)
+			{
+				var code = prog[ip];
 
-				case "<":
-					if (this.dp === 0) throw new RangeError("Attempted to move beyond lower limit of memory");
-					this.dp--;
-					break;
+				switch (code) {
+					case ">":
+						if (this.dp === (this.memory.length-1)) throw new RangeError("Attempted to move beyond upper limit of memory");
+						this.dp++;
+						break;
 
-				case "+":
-					this.memory[this.dp]++;
-					break;
+					case "<":
+						if (this.dp === 0) throw new RangeError("Attempted to move beyond lower limit of memory");
+						this.dp--;
+						break;
 
-				case "-":
-					this.memory[this.dp]--;
-					break;
+					case "+":
+						this.memory[this.dp]++;
+						break;
 
-				case ".":
-					this.io.putch(this.memory[this.dp]);
-					break;
+					case "-":
+						this.memory[this.dp]--;
+						break;
 
-				case ",":
-					this.memory[this.dp] = this.io.getch();
-					break;
+					case ".":
+						this.io.putch(this.memory[this.dp]);
+						break;
 
-				case "[":
-					if (this.memory[this.dp] === 0) {
-						loopCounter = 1
-						while (loopCounter !== 0) {
-							ip++;
-							if (prog[ip] === '[') loopCounter++;
-							else if (prog[ip] === ']') loopCounter--;
+					case ",":
+						this.memory[this.dp] = this.io.getch();
+						break;
+
+					case "[":
+						if (this.memory[this.dp] === 0) {
+							loopCounter = 1
+							while (loopCounter !== 0) {
+								ip++;
+								if (prog[ip] === '[') loopCounter++;
+								else if (prog[ip] === ']') loopCounter--;
+							}
 						}
-					}
-					break;
+						break;
 
-				case "]":
-					if (this.memory[this.dp] !== 0) {
-						loopCounter = 1;
-						while (loopCounter !== 0) {
-							ip--;
-							if (prog[ip] == '[') loopCounter--;
-							else if (prog[ip] === ']') loopCounter++;
+					case "]":
+						if (this.memory[this.dp] !== 0) {
+							loopCounter = 1;
+							while (loopCounter !== 0) {
+								ip--;
+								if (prog[ip] == '[') loopCounter--;
+								else if (prog[ip] === ']') loopCounter++;
+							}
 						}
-					}
-					break;
-			};
+						break;
+				};
 
-			ip++;
+				ip++;
+			}
+			
 		}
-	};
-
-
-	window.RefVM = RefVM;
+	});
 
 })();
--- test/testfiles.js
+++ test/testfiles.js
@@ -1,6 +1,9 @@
 (function () {
 	"use strict";
 
+	// External libs
+	Test.addScripts("../libs/ChickenFW/js/mix.js", "../libs/ChickenFW/js/namespace.js", "../libs/ChickenFW/js/class.js");
+
 	//              System under test                   Test script
 	Test.addScripts("../src/refvm.js",					"refvm.tests.js");
 })();
